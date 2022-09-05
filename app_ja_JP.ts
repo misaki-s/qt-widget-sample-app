67523ed933fdae7346f7b8c4695019bb4856a26b
Misaki Shibata <shibatam@photron.co.jp>
2022-09-05 12:15:43 +0900

    json書き込み処理追加

--- app_ja_JP.ts
+++ app_ja_JP.ts
@@ -1,3 +1,95 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ja_JP"></TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="widget.cpp" line="46"/>
+        <location filename="widget.cpp" line="70"/>
+        <source>テキスト(*.txt)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="49"/>
+        <location filename="widget.cpp" line="73"/>
+        <source>ファイルを開く</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="51"/>
+        <location filename="widget.cpp" line="75"/>
+        <source>すべて(*.*);;テキスト(*.txt);;ソース(*.h *.cpp)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Widget2</name>
+    <message>
+        <source>Widget</source>
+        <translation type="vanished">自動化ツール</translation>
+    </message>
+    <message>
+        <source>TextLabel</source>
+        <translation type="vanished">自動化ツール</translation>
+    </message>
+    <message>
+        <source>choose NMAPSSelector</source>
+        <translation type="vanished">CLIツールのconffig.jsonを選択してください</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="25"/>
+        <location filename="widget.ui" line="43"/>
+        <source>AutomationTool</source>
+        <translation type="unfinished">自動化ツール</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="55"/>
+        <source>Setting</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="66"/>
+        <source>choose AutomationTool config.json</source>
+        <translation type="unfinished">自動化CLIツールのconfig.jsonを選択してください</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="104"/>
+        <location filename="widget.ui" line="144"/>
+        <source>file</source>
+        <translation>ファイルを選択</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="118"/>
+        <source>choose NMAPSSelector.exe</source>
+        <translation type="unfinished">NMAPSSelector.exeを選択してください。</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="158"/>
+        <source>Choose Preset</source>
+        <translation type="unfinished">プリセットを選択してください。</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="206"/>
+        <source>save</source>
+        <translation type="unfinished">保存</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="271"/>
+        <source>Execute</source>
+        <translation type="unfinished">実行</translation>
+    </message>
+    <message>
+        <source>aaa</source>
+        <translation type="obsolete">あああ</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="246"/>
+        <source>AutoMation</source>
+        <translation>自動化CLI</translation>
+    </message>
+    <message>
+        <source>PushButton</source>
+        <translation type="vanished">実行</translation>
+    </message>
+</context>
+</TS>
--- widget.cpp
+++ widget.cpp
@@ -1,26 +1,37 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include <iostream>
+#include <QString>
 #include <QHBoxLayout>
+#include <QJsonObject>
+#include <QJsonParseError>
+#include <QJsonArray>
+#include <QByteArray>
+
+#include <fstream>
 Widget::Widget(QWidget *parent)
     : QWidget(parent), ui(new Ui::Widget2)
 {
     ui->setupUi(this);
 
-    ui->label->setText("Hellow World");
+    //    ui->label->setText("Hellow World");
     //    ui->pushButton
 
 
-    QHBoxLayout* l_row1 = new QHBoxLayout;
-    l_row1->addWidget(ui->lineEdit_NMAPSSelector);
-    l_row1->addWidget(ui->pushButton);
+    //    QHBoxLayout* l_row1 = new QHBoxLayout;
+    //    l_row1->addWidget(ui->lineEdit_NMAPSSelector);
+    //    l_row1->addWidget(ui->pushButton);
 
-    QHBoxLayout* l_row2 = new QHBoxLayout;
-    l_row2->addWidget(ui->lineEdit_NMAPSSelector_2);
-    l_row2->addWidget(ui->pushButton_2);
+    //    QHBoxLayout* l_row2 = new QHBoxLayout;
+    //    l_row2->addWidget(ui->lineEdit_NMAPSSelector_2);
+    //    l_row2->addWidget(ui->pushButton_2);
 
-    window()->setLayout(l_row1);
-    window()->setLayout(l_row2);
+    //    window()->setLayout(l_row1);
+    //    window()->setLayout(l_row2);
+
+    ui->combo1->addItem("Fox1");
+    ui->combo1->addItem("Fox2");
+    ui->combo1->addItem("Fox3");
 }
 
 Widget::~Widget()
@@ -37,15 +48,46 @@ void print(const T &value)
 void Widget::slot1()
 {
 
+    // QtでJsonを扱うためのオブジェクトを作製し、キーとそれに対応する値を保存する
+    QJsonObject jsonObj;
+    jsonObj["name"] = "namae";
+    jsonObj["labelKey"] = "aaa";
 
-    QString selFilter = tr("テキスト(*.txt)");
+    //QJsonDocument jsonDoc(jsonObj);
+    QJsonDocument saveDoc(jsonObj);
+
+    //QJsonDocumentインスタンスからQByteArrayを取得(JSON形式)
+    //    QByteArray data(jsonDoc.toJson());
+
+    QJsonParseError parseError;
+
+
+    if (parseError.error != QJsonParseError::NoError) {
+        qDebug() << "Error: invalid Json format";
+        return;
+    }
+
+    QFile saveFile(QStringLiteral("save.json"));
+
+    // save.jsonを開くことができなければ警告を出す
+    if (!saveFile.open(QIODevice::WriteOnly))
+    {
+        qWarning("Couldn't open save.json");
+        return;
+    }
+
+    // saveFile.open(QIODevice::WriteOnly);
+    saveFile.write(saveDoc.toJson());
+    saveFile.close();
+
+    QString selFilter = tr("テキスト(*.*)");
     QString fileName = QFileDialog::getOpenFileName(
-        this,
-        tr("ファイルを開く"),
-        "C:/",
-        tr("すべて(*.*);;テキスト(*.txt);;ソース(*.h *.cpp)"),
-        &selFilter,
-        QFileDialog::DontUseCustomDirectoryIcons);
+                this,
+                tr("ファイルを開く"),
+                "C:/",
+                tr("すべて(*.*);;テキスト(*.txt);;ソース(*.h *.cpp)"),
+                &selFilter,
+                QFileDialog::DontUseCustomDirectoryIcons);
     if (fileName.isEmpty())
     {
         // キャンセル
@@ -64,12 +106,12 @@ void Widget::slot2()
 {
     QString selFilter = tr("テキスト(*.txt)");
     QString fileName = QFileDialog::getOpenFileName(
-        this,
-        tr("ファイルを開く"),
-        "C:/",
-        tr("すべて(*.*);;テキスト(*.txt);;ソース(*.h *.cpp)"),
-        &selFilter,
-        QFileDialog::DontUseCustomDirectoryIcons);
+                this,
+                tr("ファイルを開く"),
+                "C:/",
+                tr("すべて(*.*);;テキスト(*.txt);;ソース(*.h *.cpp)"),
+                &selFilter,
+                QFileDialog::DontUseCustomDirectoryIcons);
     if (fileName.isEmpty())
     {
         // キャンセル
